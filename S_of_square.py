f69716d95cf7a0c4161838bdead028392e5676f7
merikhudaverdyan <khudaverdyanmeri36@gmail.com>
2024-12-20 15:29:37 +0400

    Update S_of_square.py

--- S_of_square.py
+++ S_of_square.py
@@ -1,11 +1,16 @@
 import math
-def sum(x1,x2,y1,y2):
-    res=(x1+x2)**2+(y1+y2)**2
-    sqrt= math.sqrt(res)
-    Sum = sqrt ** 2 / 2
-    return Sum
+
+def sum(x1,y1,x2,y2):
+  res = (x1-x2)**2 + (y1-y2)**2
+  sqroot=math.sqrt(res)
+  
+  Sum = (sqroot**2) / 2
+  return Sum
+
 x1 = int(input("Enter x1: "))
 y1 = int(input("Enter y1: "))
 x2 = int(input("Enter x2: "))
 y2 = int(input("Enter y2: "))
-Sum = sum(x1,x2,y1,y2)
+
+Sum=sum(x1,y1,x2,y2)
+print(Sum)
